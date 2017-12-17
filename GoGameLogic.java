e07d540a4d5aba61ff5b09a94172f6d7faffec99
Joe <josephcheevers@gmail.com>
2017-12-17 23:58:43 +0000

    Update GoGameLogic.java

--- GoGameLogic.java
+++ GoGameLogic.java
@@ -169,9 +169,9 @@ public class GoGameLogic {
 			// Printout the current board
 				System.out.println(goBoard.getRender());
 			// Printout the old board 
-				System.out.println(renders.get(renderCurrent-1));
+				System.out.println(renders.get(renderCurrent));
 			// Compare the old board to the new board and return true if they match
-				if(goBoard.getRender() == renders.get(renderCurrent-1))
+				if(goBoard.getRender() == renders.get(renderCurrent))
 					return true;
 			// If the move is not allowed make sure to undo the move
 				undo();
